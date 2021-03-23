df03c1ee34efdd36e7b2223724e7f32de67168c8
developer-junaid <qjunaid623@gmail.com>
2021-03-23 12:31:25 +0500

    Added Files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ textAlign: "center", fontFamily: "sans-serif" }}>
+      <h2>Hello world</h2>
+      <p>This is a tutorial to deploy gatsbyJs site to netlify</p>
+    </div>
+  )
 }
